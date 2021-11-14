395d49f332e07213d48645c46e926376fc8a89ad
aswanym <aswanymahendran222@gmail.com>
2021-11-14 08:41:21 +0530

    update on index page

--- account/views.py
+++ account/views.py
@@ -117,9 +117,14 @@ def logout(request):
 
 @never_cache
 def index(request):
+
     product = Product.objects.all()
+    paginator = Paginator(product,8)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'product': product,
+        'product': page_obj,
     }
     return render(request, 'account/index.html', context)
 
